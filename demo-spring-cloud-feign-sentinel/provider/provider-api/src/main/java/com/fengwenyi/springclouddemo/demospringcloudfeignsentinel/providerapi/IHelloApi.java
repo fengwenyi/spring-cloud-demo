275ced7d234a951f2eee121835d2f6074030536b
冯文议 <xfsy_2015@163.com>
2021-12-09 10:24:43 +0800

    修复feign api
    @RequestMapping annotation not allowed on @FeignClient interfaces

--- demo-spring-cloud-feign-sentinel/provider/provider-api/src/main/java/com/fengwenyi/springclouddemo/demospringcloudfeignsentinel/providerapi/IHelloApi.java
+++ demo-spring-cloud-feign-sentinel/provider/provider-api/src/main/java/com/fengwenyi/springclouddemo/demospringcloudfeignsentinel/providerapi/IHelloApi.java
@@ -3,16 +3,11 @@ package com.fengwenyi.springclouddemo.demospringcloudfeignsentinel.providerapi;
 import com.fengwenyi.api.result.ResponseTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- * @author <a href="https://www.fengwenyi.com">Erwin Feng</a>
- * @since 2021-12-08
- */
-@RequestMapping("/hello")
+
 public interface IHelloApi {
 
-    @GetMapping("/hi/{name}")
+    @GetMapping("/hello/{name}")
     ResponseTemplate<String> sayHi(@PathVariable("name") String name);
 
 }
