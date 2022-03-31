2696ba39ec5398836c8cc48192e2f700eeb5cb0e
冯文议 <xfsy_2015@163.com>
2022-03-31 18:11:24 +0800

    spring cloud alibaba sentinel

---
+++ demo-spring-cloud-alibaba-sentinel/README.md
@@ -0,0 +1,6 @@
+# Spring Cloud Alibaba Sentinel
+
+## Links
+
+- [Sentinel wiki](https://github.com/alibaba/Sentinel/wiki/介绍)
+- [Sentinel 控制台 wiki](https://github.com/alibaba/Sentinel/wiki/控制台)
---
+++ demo-spring-cloud-alibaba-sentinel/src/main/java/com/fengwenyi/springclouddemo/demospringcloudalibabasentinel/DemoSpringCloudAlibabaSentinelApplication.java
@@ -0,0 +1,17 @@
+package com.fengwenyi.springclouddemo.demospringcloudalibabasentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author <a href="https://www.fengwenyi.com">Erwin Feng</a>
+ * @since 2022-03-31
+ */
+@SpringBootApplication
+public class DemoSpringCloudAlibabaSentinelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringCloudAlibabaSentinelApplication.class, args);
+    }
+
+}
