0f533a0e03f169c9fa978b9da3d977f6b3463a19
Erwin <xfsy_2015@163.com>
2022-05-09 23:00:20 +0800

    spring cloud gateway init

--- demo-spring-cloud-gateway/README.md
+++ demo-spring-cloud-gateway/README.md
@@ -6,3 +6,17 @@
 ### Predicate 使用实例
 
 - org.springframework.cloud.gateway.handler.predicate.PathRoutePredicateFactory
+
+
+## 构成
+
+- Route
+- Predicate
+- Filter
+
+## 动态路由
+
+## 过滤器
+
+- 全局过滤器
+- 局部过滤器
---
+++ demo-spring-cloud-gateway/src/main/java/com/fengwenyi/springcloud/gateway/DemoGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.fengwenyi.springcloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author <a href="https://www.fengwenyi.com">Erwin Feng</a>
+ * @since 2022-05-09
+ */
+@SpringBootApplication
+public class DemoGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGatewayApplication.class, args);
+    }
+
+}
