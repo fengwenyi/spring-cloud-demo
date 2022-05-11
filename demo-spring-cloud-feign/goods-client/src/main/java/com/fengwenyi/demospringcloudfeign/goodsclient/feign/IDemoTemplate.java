447a6db008c04d1a205ee327fb9120dfa091d745
冯文议 <xfsy_2015@163.com>
2022-05-11 15:56:52 +0800

    feign decoder

--- demo-spring-cloud-feign/goods-client/src/main/java/com/fengwenyi/demospringcloudfeign/goodsclient/feign/IDemoTemplate.java
+++ demo-spring-cloud-feign/goods-client/src/main/java/com/fengwenyi/demospringcloudfeign/goodsclient/feign/IDemoTemplate.java
@@ -3,6 +3,7 @@ package com.fengwenyi.demospringcloudfeign.goodsclient.feign;
 import com.fengwenyi.api.result.PageRequestVo;
 import com.fengwenyi.api.result.PageResponseVo;
 import com.fengwenyi.api.result.ResponseTemplate;
+import com.fengwenyi.demospringcloudfeign.goodsclient.decoder.FeignDecoder;
 import com.fengwenyi.demospringcloudfeign.goodsclient.vo.request.Params;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.cloud.openfeign.SpringQueryMap;
@@ -12,7 +13,10 @@ import org.springframework.web.bind.annotation.GetMapping;
  * @author <a href="https://www.fengwenyi.com">Erwin Feng</a>
  * @since 2022-04-24
  */
-@FeignClient("demo")
+@FeignClient(
+        value = "demo"
+//        , configuration = FeignDecoder.class
+)
 public interface IDemoTemplate {
 
     @GetMapping("/demo")
