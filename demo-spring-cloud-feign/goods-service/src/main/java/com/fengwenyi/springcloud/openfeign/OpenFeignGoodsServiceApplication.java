8ad8124b41328c8241186d36485342b1ebaebb49
ErwinFeng <xfsy_2015@163.com>
2022-11-15 18:00:58 +0800

    重新构造 spring cloud openfeign example

---
+++ demo-spring-cloud-feign/goods-service/src/main/java/com/fengwenyi/springcloud/openfeign/OpenFeignGoodsServiceApplication.java
@@ -0,0 +1,17 @@
+package com.fengwenyi.springcloud.openfeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author <a href="https://fengwenyi.com">Erwin Feng</a>
+ * @since 2022-11-15
+ */
+@SpringBootApplication
+public class OpenFeignGoodsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenFeignGoodsServiceApplication.class, args);
+    }
+
+}
