2fabce6181ef0bd7ef5e34a0e8098ddeedc8cf2e
ErwinFeng <xfsy_2015@163.com>
2024-01-22 10:57:25 +0800

    feat(nacos-config): demo

---
+++ demo-spring-cloud-nacos/demo-spring-cloud-nacos-config/src/main/java/com/fengwenyi/demo/springcloud/nacos/config/NacosConfigApplication.java
@@ -0,0 +1,17 @@
+package com.fengwenyi.demo.springcloud.nacos.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author <a href="https://fengwenyi.com">Erwin Feng</a>
+ * @since 2024-01-22
+ */
+@SpringBootApplication
+public class NacosConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigApplication.class, args);
+    }
+
+}
